3bec70fab44b7fb4a17b446c9a1a0a394f9d339d
Arthur Barré <arthur.barre@easy.movie>
2020-05-18 17:03:19 +0200

    init project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld @updateMessage="title = $event" :msg="title" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      title: "ceci",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
---
+++ src/views/Listing.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="listing">
+    <h1>Listing page</h1>
+    
+  </div>
+</template>
+
+
