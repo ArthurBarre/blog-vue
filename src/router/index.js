7d9054d224d889b8b1fb765f56ccc66cba69e052
Arthur Barré <arthur.barre@easy.movie>
2020-05-19 09:14:17 +0200

    add listing

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Article from "../views/Article.vue";
+import Listing from "../views/Listing.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/article/:slug",
     name: "Article",
     component: Article,
+  },
+  {
+    path: "/listing/",
+    name: "Listing",
+    component: Listing,
   },
 ];
 
--- src/views/Listing.vue
+++ src/views/Listing.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="listing">
     <h1>Listing page</h1>
-    
+    {{ posts }}
+    <div class="content">
+      
+    </div>
   </div>
 </template>
 
-
+<script>
+export default {
+  data() {
+    return { posts: null };
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => (this.posts = data));
+  },
+};
+</script>
