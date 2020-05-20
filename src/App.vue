d645d2d96d0e43e8bfa00a047a29a000ac229165
shatice <sarah.gungor@hetic.net>
2020-05-20 02:13:44 +0200

    add font-family + clean up

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
 
 /***** VENDOR RESET *****/
 
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;0,800;0,900;1,500;1,600;1,700;1,800;1,900&display=swap");
+
 * {
   box-sizing: border-box;
 } 
@@ -69,7 +71,7 @@ a {
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #000000;
